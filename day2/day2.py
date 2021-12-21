2d8821198ac0543d587f0d260afa761c21ac2871
olli <oliver.linnenbach@protonmail.com>
2021-12-21 21:03:06 +0100

    day 2 solved

--- day2/day2.py
+++ day2/day2.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
 def main():
-    pass
+    """ provides solution for aoc 2021 day 2
+    """
+    horizontal = 0
+    depth = 0
+    with open("day_input.txt", encoding="utf-8") as day_input:
+        for line in day_input:
+            direction_steps = line.strip().split(" ")
+            if "forward" in direction_steps[0]:
+                horizontal = horizontal + int(direction_steps[1])
+            elif "down" in direction_steps[0]:
+                depth = depth + int(direction_steps[1])
+            else:
+                depth = depth - int(direction_steps[1])
+    result = horizontal * depth
+    print(result)
 
 if __name__ == '__main__':
     main()
